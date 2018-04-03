d524085ff4e243810e0d1bb5a5a22c73b596c7ea
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2018-04-03 11:13:12 +0430

    improve example 1

--- src/main-1.js
+++ src/main-1.js
@@ -5,8 +5,12 @@ import { REQUEST_DELAY } from './constants'
 import { log } from './utils'
 
 const update = async (i) => {
-    log(i, 'start')
     await setValue((await getValue(i)) + 1, i)
+}
+
+const handleReqeust = async (i) => {
+    log(i, 'start')
+    await update(i)
     log(i, 'end')
 }
 
@@ -14,7 +18,7 @@ const main = async () => {
     for (let i = 0; i < 100; i++) {
         const d = await networkDelay(REQUEST_DELAY)
         log(i, 'delay', chalk.gray(d + 'ms'))
-        update(i) // without awit :D
+        handleReqeust(i) // without awit :D
     }
     await timeout(5 * REQUEST_DELAY)
     console.log(await getValue(0))
